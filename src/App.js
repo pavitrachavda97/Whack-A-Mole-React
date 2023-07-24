436df677100ad6ba241508e8bba604925544788d
PAVITRA CHAVDA <pavitrachavda97@gmail.com>
2023-07-24 08:38:45 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MoleImage from './mole-image.jpg';
+import MoleHitImage from './mole-hit-image.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [score, setScore] = useState(0);
+    const [activeMole, setActiveMole] = useState(null);
+    const holes = [...Array(9).keys()];
+
+    useEffect(() => {
+        startGame();
+    }, []);
+
+    const startGame = () => {
+        setScore(0);
+        setActiveMole(null);
+        showMole();
+    };
+
+    const showMole = () => {
+        if (activeMole !== null) 
+        {
+            return;
+        }
+
+        const randomHoleIndex = Math.floor(Math.random() * holes.length);
+        setActiveMole(randomHoleIndex);
+
+        setTimeout(() => {
+            setActiveMole(null);
+            if (holes.length > 1) 
+            {
+                showMole();
+            }
+        }, 1000);
+    };
+
+    const handleHit = (holeIndex) => {
+        if (holeIndex === activeMole) {
+            setScore((prevScore) => prevScore + 1);
+            setActiveMole(null);
+        }
+    };
+
+    return (
+        <div className="App">
+            <p className="score">Score : <span> {score} </span></p>
+            <div className="container">
+                {
+                    holes.map((holeIndex) => (
+                    <div
+                        key = {holeIndex}
+                        className = {`hole ${holeIndex === activeMole ? 'active' : ''}`}
+                        onClick = {() => handleHit(holeIndex)} >
+                        {
+                            holeIndex === activeMole && (
+                            <img
+                                className="mole"
+                                src={holeIndex === activeMole ? MoleImage : MoleHitImage}
+                                alt="Mole"
+                            />
+                            )
+                        }
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
